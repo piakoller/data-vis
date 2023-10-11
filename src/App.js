f1f4200c17eee4661bfa3940340ee0ad6060c664
filipacchi <ludvig.bennbom@gmail.com>
2023-10-11 16:12:27 +0200

    basic chart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import vegaEmbed from "vega-embed";
+import { useRef, useEffect } from "react";
 
 function App() {
+  const vegaLiteSpec = {
+    "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+    "data": {
+      "values": [
+        {"category": "A", "count": 28},
+        {"category": "B", "count": 55},
+        {"category": "C", "count": 43},
+        {"category": "D", "count": 91},
+        {"category": "E", "count": 81},
+        {"category": "F", "count": 28},
+        {"category": "G", "count": 22},
+        {"category": "H", "count": 99},
+        {"category": "I", "count": 5},
+        {"category": "J", "count": 54},
+      ],
+    },
+    "mark": "bar",
+    "encoding": {
+      "x": {"field": "category", "type": "ordinal"},
+      "y": {"field": "count", "type": "quantitative"},
+    },
+  };
+
+  const VegaLiteChart = ({ spec }) => {
+    const containerRef = useRef(null);
+  
+    useEffect(() => {
+      if (containerRef.current) {
+        vegaEmbed(containerRef.current, spec);
+      }
+    }, [spec]);
+  
+    return <div ref={containerRef}></div>;
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <VegaLiteChart spec={vegaLiteSpec} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +56,7 @@ function App() {
           Learn React
         </a>
       </header>
+      
     </div>
   );
 }
