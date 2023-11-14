0d81a01a06d168578ae35614956ee9723f7fbae7
ru48tak <koller.pia@campus.lmu.de>
2023-11-14 12:45:22 +0100

    small design updates

--- src/App.js
+++ src/App.js
@@ -8,34 +8,34 @@ function App() {
     "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
     "data": {
       "values": [
-        {"category": "A", "count": 28},
-        {"category": "B", "count": 55},
-        {"category": "C", "count": 43},
-        {"category": "D", "count": 91},
-        {"category": "E", "count": 81},
-        {"category": "F", "count": 28},
-        {"category": "G", "count": 22},
-        {"category": "H", "count": 99},
-        {"category": "I", "count": 5},
-        {"category": "J", "count": 54},
+        { "category": "A", "count": 28 },
+        { "category": "B", "count": 55 },
+        { "category": "C", "count": 43 },
+        { "category": "D", "count": 91 },
+        { "category": "E", "count": 81 },
+        { "category": "F", "count": 28 },
+        { "category": "G", "count": 22 },
+        { "category": "H", "count": 99 },
+        { "category": "I", "count": 5 },
+        { "category": "J", "count": 54 },
       ],
     },
     "mark": "bar",
     "encoding": {
-      "x": {"field": "category", "type": "ordinal"},
-      "y": {"field": "count", "type": "quantitative"},
+      "x": { "field": "category", "type": "ordinal" },
+      "y": { "field": "count", "type": "quantitative" },
     },
   };
 
   const VegaLiteChart = ({ spec }) => {
     const containerRef = useRef(null);
-  
+
     useEffect(() => {
       if (containerRef.current) {
         vegaEmbed(containerRef.current, spec);
       }
     }, [spec]);
-  
+
     return <div ref={containerRef}></div>;
   };
 
@@ -43,20 +43,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <VegaLiteChart spec={vegaLiteSpec} />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="parent">
+          <div className="box1">
+            <VegaLiteChart spec={vegaLiteSpec} />
+          </div>
+          <div className="box2">
+            BOX 2
+          </div>
+          <div className="box3">
+            BOX 3
+          </div>
+          <div className="box4">
+            BOX 3
+          </div>
+        </div>
       </header>
-      
+
     </div>
   );
 }
